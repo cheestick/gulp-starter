2a5f2623eab045cff8f7660b71e9da46db9e3391
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2024-08-01 19:48:07 +0300

    browser sync was added and configured

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ global.app = {
 import { copy } from "./gulp/tasks/copy.js";
 import { reset } from "./gulp/tasks/reset.js";
 import { html } from "./gulp/tasks/html.js";
+import { server } from "./gulp/tasks/server.js";
 
 function watcher() {
   gulp.watch(path.watch.files, copy);
@@ -19,7 +20,7 @@ function watcher() {
 
 const mainTasks = gulp.parallel(copy, html);
 
-const dev = gulp.series(reset, mainTasks, watcher);
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 
 gulp.task("clean", reset);
 gulp.task("default", dev);
